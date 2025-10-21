44e7424a73d994811c34e231281cfeaaf39569f5
zak-dev-art <zachndungu861@gmail.com>
2025-10-21 09:09:54 +0300

    modified api

--- src/api/botsApi.js
+++ src/api/botsApi.js
@@ -6,6 +6,8 @@ export async function getBots() {
   return res.json();
 }
 
+
+
 export async function deleteBot(id) {
   const res = await fetch(`${BASE}/bots/${id}`, { method: "DELETE" });
   if (!res.ok) throw new Error("Failed to delete bot");
